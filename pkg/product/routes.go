25d95e32062a411d0c4a5316a3fa89c7c1cb5989
Antonio Bello <ab3llo15@gmail.com>
2022-04-01 21:58:55 +0100

    feat: Add product service

---
+++ pkg/product/routes.go
@@ -0,0 +1,28 @@
+package product
+
+import (
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/auth"
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/config"
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/product/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config, authSvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware()
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routes := r.Group("/api/products")
+	routes.Use(a.AuthRequired)
+	routes.POST("/", svc.CreateProduct)
+	routes.GET("/:id", svc.FindOne)
+}
+
+func (svc *ServiceClient) CreateProduct(ctx *gin.Context) {
+	routes.CreateProduct(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) FindOne(ctx *gin.Context) {
+	routes.FindOne(ctx, svc.Client)
+}
---
+++ pkg/product/routes/find_one.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/product/pb"
+	"github.com/gin-gonic/gin"
+)
+
+func FindOne(ctx *gin.Context, c pb.ProductServiceClient) {
+	id, _ := ctx.Params.Get("id")
+
+	res, err := c.FindOne(context.Background(), &pb.FindOneRequest{
+		ProductId: id,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
