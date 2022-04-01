ec1523f07c3f3a3c64415b9e3536f79710e8d853
Antonio Bello <ab3llo15@gmail.com>
2022-04-01 21:24:21 +0100

    feat: Add auth service

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthMiddlewareConfig struct {
+	svc *ServiceClient
+}
+
+func InitAuthMiddleware(svc *ServiceClient) AuthMiddlewareConfig {
+	return AuthMiddlewareConfig{svc}
+}
+
+func (c *AuthMiddlewareConfig) AuthRequired(ctx *gin.Context) {
+	authToken := ctx.Request.Header.Get("authorization")
+
+	if authToken == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.Split(authToken, "Bearer ")
+
+	if len(token) < 2 {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	res, err := c.svc.Client.Validate(context.Background(), &pb.ValidateRequest{
+		Token: token[1],
+	})
+
+	if err != nil || res.Status != http.StatusOK {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	ctx.Set("userId", res.UserId)
+	ctx.Next()
+}
---
+++ pkg/auth/routes/register.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ab3llo/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+)
+
+type RegisterRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Register(ctx *gin.Context, c pb.AuthServiceClient) {
+	body := RegisterRequestBody{}
+
+	if err := ctx.Bind(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Register(context.Background(), &pb.RegisterRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+	}
+
+	ctx.JSON(int(res.Status), &res)
+}
